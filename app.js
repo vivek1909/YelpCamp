814f6e86dbb1696da0ca199ab3a2b67956ff8991
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-02-20 23:29:52 +0530

    .

--- app.js
+++ app.js
@@ -15,17 +15,22 @@ app.get("/campgrounds", function(req, res) {
         {name: "Salmon Creek", image: "https://www.flickr.com/photos/scpetrel/14301524356/"},
         {name: "Granite Hill", image: "https://www.flickr.com/photos/scpetrel/14301524356/"},
         {name: "Mountain Goat's Rest", image: "https://www.flickr.com/photos/scpetrel/14301524356/"}
-    ]
+    ];
     res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 app.post("/campgrounds", function(req, res) {
-    //get data from form and add to campgrounds array
+    //get data from form and push it into the campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
     //redirect back to campgrounds page
+    res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res) {
-    
+    res.render("new");
 });
 
 app.listen(3000, function(){
