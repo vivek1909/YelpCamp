cd4dbe470863f5d3e8fcf11fb3f55f3e0e59c4ee
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2019-02-26 00:53:23 +0530

    .

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-    {name: "Salmon Creek", image: "https://www.flickr.com/photos/scpetrel/14301524356/"},
-    {name: "Granite Hill", image: "https://www.flickr.com/photos/scpetrel/14301524356/"},
-    {name: "Mountain Goat's Rest", image: "https://www.flickr.com/photos/scpetrel/14301524356/"}
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1542841366-c08a2567da87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1542841366-c08a2567da87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
+    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1542841366-c08a2567da87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"}
 ];
 
 app.get("/", function(req, res) {
